e5c4f78f97550b213eddea9716f0988a570128d2
Shaul Kimel <shaullk600@gmail.com>
2025-07-30 12:11:54 +0300

    decomention: func 'hashPass' file: utils.service

--- src/utils/utils.service.ts
+++ src/utils/utils.service.ts
@@ -4,8 +4,14 @@ import bcrypt from "bcrypt";
 @Injectable()
 export class UtilsService {
 
+    /**
+     * a function to return hashing in password
+     * @param pass {string}
+     * @returns hashPass {string}
+     */
     hashPass(pass:string) {
         return bcrypt.hashSync(pass , 10);
     }
+
 
 }
