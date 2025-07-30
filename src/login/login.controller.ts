ad5bbe06287b37cb5526f37ff609588b5875c387
Shaul Kimel <shaullk600@gmail.com>
2025-07-30 11:52:21 +0300

    decomention: function login

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -5,7 +5,12 @@ import { LoginService } from "./login.service";
 export class LoginController {
     constructor(private loginService: LoginService) { }
 
-
+    /**
+     * 
+     * @param user_name 
+     * @param pass 
+     * @returns function to serviced
+     */
     @Post('/')
     loginA(@Headers('user_name') user_name: string, @Headers('pass') pass: string): string {
         return this.loginService.loginS(user_name, pass);
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -3,8 +3,16 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class LoginService {
 
+    /**
+     * 
+     * @param user_name 
+     * @param pass 
+     * @returns string
+     */
     loginS(user_name:string , pass:string){
         console.log(user_name + " " + pass);
         return 'jgjgj'
     }
+
+    
 }
