41d86010cde9dbfdd2753222c19a94caf1f89d08
Shaul Kimel <shaullk600@gmail.com>
2025-07-30 11:54:47 +0300

    create: file'utils'

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
 
 import { ConfigModule } from "@nestjs/config";
+import { UtilsModule } from './utils/utils.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), LoginModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), LoginModule, UtilsModule],
   controllers: [AppController],
   providers: [AppService],
 })
