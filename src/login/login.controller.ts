08368c827a3c790a59698cc76edd4573fe89032a
Shaul Kimel <shaullk600@gmail.com>
2025-07-30 11:39:31 +0300

    update: API's first

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Headers, Post } from '@nestjs/common';
 import { LoginService } from "./login.service";
 
 @Controller('login')
 export class LoginController {
     
+    @Post('/')
+    login(@Headers('user_name') user_name , @Headers('pass') pass) : string{
+        console.log(user_name + " " + pass);
+        return 'godjun';
+    }
 }
